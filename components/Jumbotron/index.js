59e182cedfc9f66dfed06301e65964322e754b0e
dany ramirez <drz0224@gmail.com>
2022-11-17 20:42:00 -0800

    added button with inputGroup

--- components/Jumbotron/index.js
+++ components/Jumbotron/index.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { FormControl } from "react-bootstrap";
+import './styles.css'
+import { InputGroup, FormControl, Button } from "react-bootstrap"; 
 
 function myJumbotron() {
-    return <div></div>
+    return {
+        <>
+        <InputGroup className="mb-3">
+        <Form.Control
+          placeholder="Recipient's username"
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+        />
+        <Button variant="outline-secondary" id="button-addon2">
+          Button
+        </Button>
+      </InputGroup>
+        </>
+    };
 }
 
 export default myJumbotron;
