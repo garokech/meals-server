684614664cd73665261bf9f580579cd42eb2788d
dany ramirez <drz0224@gmail.com>
2022-11-17 21:16:12 -0800

    added css file

--- components/Jumbotron/index.js
+++ components/Jumbotron/index.js
@@ -1,13 +1,12 @@
 import React from "react";
-import { FormControl } from "react-bootstrap";
-import './styles.css'
+import './styles.css';
 import { InputGroup, FormControl, Button } from "react-bootstrap"; 
 
-function myJumbotron() {
-    return {
+function MyJumbotron() {
+    return (
         <>
         <InputGroup className="mb-3">
-        <Form.Control
+        <FormControl
           placeholder="Recipient's username"
           aria-label="Recipient's username"
           aria-describedby="basic-addon2"
@@ -17,7 +16,7 @@ function myJumbotron() {
         </Button>
       </InputGroup>
         </>
-    };
+    );
 }
 
-export default myJumbotron;
+export default MyJumbotron;
