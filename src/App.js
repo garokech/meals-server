115fa7c0b1819157c9b920e9fcfb06d530d226fe
dany ramirez <drz0224@gmail.com>
2022-11-22 21:45:07 -0800

    added paths

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import './App.css';
 import AppContext from './context';
 import AppNavbar from './Components/NavBar';
 import Home from './pages/Home';
+import {Route, BrouserRouter as Router, Switch} from 'react-router-dom'
 
 function App() {
   return (
+    <Router>
     <AppContext>
-    <div>
+    <div className="Jumbo">
       <AppNavbar />
-      <Home />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/login">
+          <Home />
+        </Route>
+      </Switch>
     </div>
     </AppContext>
+    </Router>
   );
 }
 
