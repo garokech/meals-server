8d7fec7e91182609be84fe7c1404fbd2fa218913
dany ramirez <drz0224@gmail.com>
2022-11-22 18:40:45 -0800

    changed font size

--- components/Jumbotron/index.js
+++ components/Jumbotron/index.js
@@ -5,6 +5,8 @@ import { InputGroup, FormControl, Button } from "react-bootstrap";
 function MyJumbotron() {
     return (
         <div className="my-jumbotron">
+          <h1>Welcome</h1>
+          <h2>You can search your favorite meals</h2>
           <div className="button-input">
         <InputGroup className="mb-3">
         <FormControl
