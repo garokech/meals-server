2993d596d2d241d8aec28f1549389c92ea45b3fd
dany ramirez <drz0224@gmail.com>
2022-11-22 18:54:12 -0800

    added imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from "./pages/Home";
+import axios from 'axios'
+import {useEffect} from 'react'
 
 function App() {
+  useEffect(() => {
+    axios
+    .get('https://www.themealab.com/api/json/v1/1/search.php?f=a')
+    .then(data => console.log(data))
+  }, [])
   return (
     <div className="jumbotron">
       <Navbar />
